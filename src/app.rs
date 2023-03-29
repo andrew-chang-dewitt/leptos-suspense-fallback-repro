67cea6ef73bd205f7e5db311bd112576f8b49466
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2023-03-29 11:56:53 -0500

    Reproduce as nested suspense issue.

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,12 @@
+use async_std::task::sleep;
+use core::time::Duration;
 use leptos::{
-    component, create_resource, view, IntoView, Scope, ServerFnError, Suspense, SuspenseProps,
+    component, create_resource, create_server_action, server,
+    server_fn::{self, ServerFn, ServerFnError},
+    view, Children, ChildrenFn, IntoView, Scope, SignalGet, Suspense, SuspenseProps,
 };
 use leptos_meta::*;
-use leptos_router::{Route, RouteProps, Router, RouterProps, Routes, RoutesProps};
+use leptos_router::{AProps, Route, RouteProps, Router, RouterProps, Routes, RoutesProps, A};
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
@@ -11,27 +15,113 @@ pub fn App(cx: Scope) -> impl IntoView {
 
     view! {
         cx,
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/suspense-fallback-repro.css"/>
-
-        // sets the document title
         <Title text="Suspense Fallback Reproduction"/>
-
-        // content for this welcome page
+        // content
         <Router>
-            <main>
-                <Routes>
-                    <Route path="" view=|cx| view! { cx, <Data/> }/>
-                </Routes>
-            </main>
+            <Routes>
+                <Route path="" view=|cx| view!{cx, <Layout>""</Layout>}/>
+                <Route path="/basic-nesting" view=|cx| view!{cx, <Layout><BasicNesting/></Layout>}/>
+                <Route path="/action-nesting" view=|cx| view!{cx, <Layout><ActionSuspense><Data/></ActionSuspense></Layout>}/>
+            </Routes>
         </Router>
     }
 }
 
 #[component]
-pub fn Data(cx: Scope) -> impl IntoView {
+fn Layout(cx: Scope, children: Children) -> impl IntoView {
+    view! {
+        cx,
+        <nav><ul>
+            <li><A href="/basic-nesting">"Basic Nesting"</A></li>
+            <li><A href="/action-nesting">"Action Suspense Nesting"</A></li>
+        </ul></nav>
+        {children(cx)}
+    }
+}
+
+#[component]
+fn BasicNesting(cx: Scope) -> impl IntoView {
+    let resource = create_resource(cx, || (), move |_| get_wrapper());
+    let data = move || resource.read(cx);
+
+    view! {
+        cx,
+        <h1>"Basic Nesting"</h1>
+        <p>"\
+            This example shows what happens when you render a Suspense inside another Suspense. When \
+            navigating to this route via client side routing (e.g. start at "<code>"/"</code>", then \
+            click on \"Basic Nesting\" above), everything behaves as expected. However, when \
+            navigating to this page via server side routing (e.g. via a hard refresh on this url or \
+            typing it in and going to it directly), the content of the Outer Suspense (the "
+            <code>"BasicNesting"</code>" component) is replaced with the content of the Inner Suspense \
+            (the "<code>"Data"</code>" component) when Data's resource resolves. \
+        "</p>
+        <Suspense fallback=move || view!{cx, <p>"Loading wrapper..."</p>}>
+            <p>"Inside wrapper suspense: "{data()}</p>
+            <Data />
+        </Suspense>
+    }
+}
+
+async fn get_wrapper() -> bool {
+    true
+}
+
+#[component]
+fn ActionSuspense(cx: Scope, children: ChildrenFn) -> impl IntoView {
+    let action = create_server_action::<B>(cx);
+    let resource = create_resource(cx, move || action.version().get(), move |_| b(cx));
+    let result = move || {
+        resource.read(cx).map(|r| {
+            log::debug!("result in ActionSuspense: {r:#?}");
+
+            match r {
+                Ok(true) => view! {
+                    cx,
+                    <p>"Result is true, rendering children:"</p>
+                    {children(cx)}
+                }
+                .into_view(cx),
+                _ => view! {
+                    cx,
+                    <p>"Result is false/err, not rendering children"</p>
+                }
+                .into_view(cx),
+            }
+        })
+    };
+
+    view! {
+        cx,
+        <h1>"Action Suspense"</h1>
+            <p>"
+                This example shows something closer to what I was dealing with in my project when \
+                I discovered this issue. When getting data from a ServerFn via an action wrapped in \
+                a resource, there is a similarity to the \"Basic Nesting\" example in that \
+                everything works as expected when navigating here via client side routing and that \
+                things begin to behave in unexpected ways via server side routing. The difference \
+                is that instead of replacing the Outer Suspense's child on the Inner Suspense's \
+                resource resolution, we see the Inner Suspense's "<code>"fallback"</code>" component \
+                isn't replaced by the child—instead it stays while the child is appended to \
+                the DOM after it.
+            "</p>
+        <Suspense fallback=move || view!{cx, <p>"Loading"</p>}>
+            {result()}
+        </Suspense>
+    }
+}
+
+#[server(B, "/api")]
+async fn b(_: Scope) -> Result<bool, ServerFnError> {
+    log::debug!("getting from server fn");
+    sleep(Duration::from_secs(2)).await;
+
+    Ok(true)
+}
+
+#[component]
+fn Data(cx: Scope) -> impl IntoView {
     let data_resource = create_resource(
         cx,
         // TODO: for now just run this resource on component load
@@ -46,19 +136,17 @@ pub fn Data(cx: Scope) -> impl IntoView {
 
     view! {
         cx,
-        <h1>"Data"</h1>
-        <Suspense fallback={ move || view!{cx, <p>"Loading..."</p>}}>
+        <h2>"Data"</h2>
+        <Suspense fallback=move || view!{cx, <p>"Loading..."</p>}>
             // FIXME:: the fallback component is never unmounted & replaced with the below child?
-            {move || data().map(|r| match r {
-                Ok(a) => view!{cx, <p>{serde_json::to_string(&a)}</p>},
-                Err(e) => view!{cx, <p>{e.to_string()}</p>},
-            })}
+            {data()}
         </Suspense>
     }
 }
 
-pub async fn get_data() -> Result<Vec<String>, ServerFnError> {
+pub async fn get_data() -> String {
     log::debug!("Getting data...");
+    sleep(Duration::from_secs(2)).await;
 
-    Ok(vec![String::from("This is Data!")])
+    String::from("This is Data!")
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn main() {
     use suspense_fallback_repro::fileserv::file_and_error_handler;
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
+    B::register().expect("Server fn to register");
 
     // Setting get_configuration(None) means we'll be using cargo-leptos's env values
     // For deployment these variables are:
